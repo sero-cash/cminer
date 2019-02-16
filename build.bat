1fb6ff12e0dcd89686bfd1da5bf426e9de26eeda
gordon-sero <gordon@sero.vip>
2019-02-17 02:09:52 +0900

    add build.bat

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+rem add MSVC in PATH
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
+
+rem add Perl in PATH; it's needed for OpenSSL build
+set "PERL_PATH=C:\Perl\perl\bin"
+set "CMAKE_PATH=C:\Program Files\CMake\bin"
+set "PATH=%PERL_PATH%;%CMAKE_PATH%;%PATH%"
+
+rem switch to ethminer's source folder
+rem cd "%~dp0\cminer\"
+
+if not exist "build\" mkdir "build\"
+
+cd build
+
+rem For CUDA 9.x pass also `-T v140`
+cmake -G "Visual Studio 15 2017 Win64" -H. -Bbuild ..
+cmake --build . --config Release --target cminer
+rem make cminer
+
+endlocal
+pause
